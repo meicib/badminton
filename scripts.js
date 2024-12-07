7d2536c5b05032a850fdfdafdae0e98c299969a2
meicib <ameicib@berkeley.edu>
2024-12-06 16:17:11 -0800

    Connected to DB, now stores values

--- scripts.js
+++ scripts.js
@@ -5,46 +5,29 @@ document.getElementById("login-button").addEventListener('click', function() {
         document.getElementById("pass").value = "";
         document.getElementById("admin-page").style.display = "block";
 
-        document.getElementById("update").addEventListener('click', function() {
+        document.getElementById("update").addEventListener('click', async function() {
         
             const capacity = document.getElementById("capacity").value;
-            console.log(typeof capacity)
             if (capacity != "") {
-                document.getElementById("capacity").value = "";
-
-                const updateContainer = document.getElementById("update-container");
-                updateContainer.style.border = "5px solid"
-                updateContainer.style.borderRadius = "5px"
-        
-                const number = document.getElementById("number");
-                number.textContent = capacity + " players";
-
                 let now = new Date();
-                let today = now.toDateString();
-                let hours = now.getHours();
-                let minutes = now.getMinutes();
-                let seconds = now.getSeconds();
+                
+                const response = await fetch('/status', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        capacity: capacity,
+                        today: now.toDateString(),
+                        hours: now.getHours(),
+                        minutes: now.getMinutes(),
+                        seconds: now.getSeconds(),
+                    })
+                });
 
-                const lastUpdated = document.getElementById("last-updated");
-                lastUpdated.textContent = `Last updated: ${today}, ${hours}:${minutes}:${seconds}`;
-        
-                const lowOrHigh = document.getElementById("summary");
-                const backgroundColor = document.getElementById("update-container")
-        
-                if (capacity <= 16) {
-                    backgroundColor.style.backgroundColor = '#e7fcf6';
-                    updateContainer.style.borderColor = '#e7fcf6';
-                    lowOrHigh.textContent = "low";
-                } else if (capacity <= 24) {
-                    backgroundColor.style.backgroundColor = '#fff9e7';
-                    updateContainer.style.borderColor = '#fff9e7';
-                    lowOrHigh.textContent = "medium";
-                } else {
-                    backgroundColor.style.backgroundColor = '#ffecee';
-                    updateContainer.style.borderColor = '#ffecee';
-                    lowOrHigh.textContent = "high";
+                const data = await response.json();
+
+                if (response.ok) {
+                    document.getElementById("capacity").value = "";
+                    await updateStatus();
                 }
-                document.getElementById("status").textContent = "success"
             }
         });
     }    
@@ -55,3 +38,42 @@ document.getElementById("back").addEventListener('click', function() {
     document.getElementById("main-page").style.display = "block";
     document.getElementById("admin-page").style.display = "none";  
 });
+
+async function updateStatus() {
+
+    const response = await fetch('/status');
+    const data = await response.json();
+
+    if (response.ok) {
+        const updateContainer = document.getElementById("update-container");
+        updateContainer.style.border = "5px solid"
+        updateContainer.style.borderRadius = "5px"
+
+        const number = document.getElementById("number");
+        number.textContent = data.body.capacity + " players";
+
+        const lastUpdated = document.getElementById("last-updated");
+        lastUpdated.textContent = `Last updated: ${data.body.today}, ${data.body.hours}:${data.body.minutes}:${data.body.seconds}`;
+
+        const lowOrHigh = document.getElementById("summary");
+        const backgroundColor = document.getElementById("update-container")
+
+        if (capacity <= 16) {
+            backgroundColor.style.backgroundColor = '#e7fcf6';
+            updateContainer.style.borderColor = '#e7fcf6';
+            lowOrHigh.textContent = "low";
+        } else if (capacity <= 24) {
+            backgroundColor.style.backgroundColor = '#fff9e7';
+            updateContainer.style.borderColor = '#fff9e7';
+            lowOrHigh.textContent = "medium";
+        } else {
+            backgroundColor.style.backgroundColor = '#ffecee';
+            updateContainer.style.borderColor = '#ffecee';
+            lowOrHigh.textContent = "high";
+        }
+        document.getElementById("status").textContent = "success"
+    } else {
+        alert('Status could not be loaded! See console.')
+        console.log(response, data)
+    }
+}
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const asyncHandler = require('express-async-handler')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const dbConnectionUri = "mongodb+srv://ameicib:yCXUFzJovU1PDGD2@cluster.qrxe0.mongodb.net/?retryWrites=true&w=majority&appName=cluster";
+const dbName = "srbc";
+
+const statusSchema = new mongoose.Schema({
+    capacity: Number,
+    day: String,
+    hours: Number,
+    minutes: Number,
+    seconds: Number
+});
+
+const Status = mongoose.model('Status', statusSchema);
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+app.get('/status', asyncHandler(async (req, res) => {
+    const status = await Status.find();
+    res.json(status)
+}));
+
+app.post('/status', asyncHandler(async (req, res) => {
+    await db.collection('inventory').deleteMany({});
+
+    const capacity = req.body.capacity;
+    const day = req.body.day;
+    const hours = req.body.hours;
+    const minutes = req.body.minutes;
+    const seconds = req.body.seconds;
+
+    const newStatus = new Status({capacity, day, hours, minutes, seconds });
+    await newStatus.save()
+
+    res.status(201).json(newStatus)
+}))
+
+async function connectToDB() {
+    await mongoose.connect(dbConnectionUri, { dbName });
+    
+    app.listen(3000, () => {
+        console.log('Server listening on port 3000')
+    })
+}
+
+connectToDB();
